bf559d32a5cc0260bfca2edcac87e323b8c617ac
ShubhamAggarwal2000 <shubhamaggarwal2808@gmail.com>
2023-11-18 23:33:02 +0530

    Integrate GPT-Neo model in backend

--- backend/app.py
+++ backend/app.py
@@ -1,49 +1,50 @@
 # Import Flask class and other dependencies like request and jsonify
 from flask import Flask, request, jsonify
+from models.model_loader import load_model
 
 # Create an instance of the Flask app
-# __name__ allows Flask to locate resources like templates relative to the app
 app = Flask(__name__)
 
-# Application configuration settings
-# Set debug mode to True for auto-reload when code changes
-app.config['DEBUG'] = True
+# Load the model
+model_path = "C:\\Users\\shubh\\OneDrive\\Desktop\\Hyperboost\\gpt-neo-125m"
+model, tokenizer = load_model(model_path)
 
-# Disable sorting JSON keys alphabetically and use dict insert order instead
-# This allows control over the response structure
+# Application configuration settings
+app.config['DEBUG'] = True
 app.config['JSON_SORT_KEYS'] = False
 
-# Add other config values as needed
+# Helper function to generate response
+
+
+def generate_response(input_text):
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    max_length = len(input_text.split()) + 50
+    output_ids = model.generate(input_ids, max_length=max_length)
+    return tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
 # Route handler for /chat endpoint
-# POST method allows sending data in request body
 
 
 @app.route("/chat", methods=["POST"])
 def chat():
     """
     Chatbot query-response route
-
-    This route handles queries sent to the chatbot
-    and returns the bot's response
     """
-    return {"response": "Hello!"}  # Logic to be added later
-
+    user_input = request.json['message']
+    response = generate_response(user_input)
+    return {"response": response}
 
 # Route handler for /train endpoint
-# POST method allows sending training data
+
+
 @app.route("/train", methods=["POST"])
 def train():
     """
     Retrain chatbot route
-
-    This route allows retraining the chatbot 
-    model on new data
     """
-    return {"status": "success"}   # Logic to be added later
+    return {"status": "success"}
 
 
 # Standard boilerplate
-# Runs the Flask app when executed directly
 if __name__ == "__main__":
     app.run()
