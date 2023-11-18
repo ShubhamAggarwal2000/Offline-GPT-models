bb13e0aeb5a38edfcf8ae560fb93af66e68d5055
ShubhamAggarwal2000 <shubhamaggarwal2808@gmail.com>
2023-11-18 23:49:17 +0530

    Frontend backend integration

--- backend/validate_install.py
+++ backend/validate_install.py
@@ -2,6 +2,7 @@ try:
     from transformers import GPTNeoForCausalLM, GPT2Tokenizer
     import torch
     import flask
+    import flask_cors
     print("All modules are installed correctly.")
 except ImportError as e:
     print(f"An error occurred: {e}")
