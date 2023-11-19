684144d1296d2eb196bf6660d2c99b6e444f8f77
ShubhamAggarwal2000 <shubhamaggarwal2808@gmail.com>
2023-11-19 15:49:36 +0530

    Added train setup for backend, need to fix frontend config

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@
 from flask import Flask, request, jsonify
 from models.model_loader import load_model
 from flask_cors import CORS
+import os
+import torch
+import io
+from werkzeug.utils import secure_filename
+
 
 
 # Create an instance of the Flask app
@@ -13,13 +18,20 @@ CORS(app)
 model_path = "C:\\Users\\shubh\\OneDrive\\Desktop\\Hyperboost\\gpt-neo-125m"
 model, tokenizer = load_model(model_path)
 
+# Define a directory to save uploaded files
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', "docx"])  # Add or remove file types as needed
+
+
 # Application configuration settings
 app.config['DEBUG'] = True
 app.config['JSON_SORT_KEYS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the upload directory exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Helper function to generate response
-
-
 def generate_response(input_text):
     input_ids = tokenizer.encode(input_text, return_tensors="pt")
     # Create an attention mask
@@ -30,9 +42,13 @@ def generate_response(input_text):
     return tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
 
+# Function to check if file extension is allowed
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 # Route handler for /chat endpoint
-
-
 @app.route("/chat", methods=["POST"])
 def chat():
     """
@@ -41,6 +57,22 @@ def chat():
     user_input = request.json['message']
     response = generate_response(user_input)
     return {"response": response}
+
+
+# Route handler for /upload endpoint
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    if 'files[]' not in request.files:
+        return jsonify({'status': 'No file part'})
+    
+    files = request.files.getlist('files[]')
+    
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return jsonify({'status': 'success'})
 
 # Route handler for /train endpoint
 
@@ -55,4 +87,5 @@ def train():
 
 # Standard boilerplate
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(port=port)
--- backend/test_app.py
+++ backend/test_app.py
@@ -1,5 +1,6 @@
 import unittest
 from app import app  # Import the Flask app instance
+import io 
 
 
 class TestApp(unittest.TestCase):
@@ -13,6 +14,19 @@ class TestApp(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             # Check if the response contains a 'response' key
             self.assertIn('response', response.json)
+            
+    def test_upload_route(self):
+        """
+        Test the /upload route
+        """
+        with app.test_client() as client:
+            data = {
+                'files[]': (io.BytesIO(b"dummy file content"), 'test.txt'),
+            }
+            response = client.post('/upload', data=data, content_type='multipart/form-data')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json, {'status': 'success'})
+        
 
     def test_train_route(self):
         """
